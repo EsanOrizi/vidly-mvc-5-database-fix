775f2d4f1e5b1b5c5aa12059ab25d3c95af9ed70
Esan <Esan@DESKTOP-T06NDV6>
2020-04-03 19:30:31 +0100

    Section 4 - Exercise solution

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -14,5 +14,19 @@ namespace Vidly.ViewModels
         public Movie Movie { get; set; }
 
 
+        // Use this logic to set title for this model view, if movie is in database title is set to edit movie otherwise is new movie
+        public string Title
+        {
+            get
+            {
+                if (Movie != null && Movie.Id != 0)
+                    return "Edit Movie";
+               
+                    return "New Movie";
+
+               
+            }
+        }
+
     }
 }
